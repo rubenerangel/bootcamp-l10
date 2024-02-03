f20b75915826724edadee5a7f8c8aed0967c22e5
Rubén Rangel <rubenrang@gmail.com>
2024-02-02 19:41:54 -0500

    Layout, navigation, send forms

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
+
+    Route::post('chirps', function() {
+        $message = request('message');
+        // Insert into database
+    });
 });
 
 require __DIR__.'/auth.php';
