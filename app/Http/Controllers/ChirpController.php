861406d1ef1496ddcf9a752aed9a5d1395ad2085
Rubén Rangel <rubenrang@gmail.com>
2024-02-04 08:09:21 -0500

    Form validation, relational DB

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('chirps.index');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => ['required', 'min:3', 'max:255']
+        ]);
+
+        // $message = $request->get('message');
+        // Insert into database
+        // Chirp::create([
+        //     'message' => $message,
+        //     'user_id' => auth()->id(),
+        // ]);
+
+        $request->user()->chirps()->create($validated);
+
+        // session()->flash('status', 'Chirp created successfully');
+
+        return to_route('chirps.index')
+            ->with('status', __('Chirp created successfully'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProfileController;
 
 Route::view('/', 'welcome')->name('welcome');
-
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
@@ -28,14 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/chirps', function () {
-        return view('chirps.index');
-    })->name('chirps.index');
+    Route::get('/chirps', [ChirpController::class, 'index'])
+        ->name('chirps.index');
 
-    Route::post('chirps', function() {
-        $message = request('message');
-        // Insert into database
-    });
+    Route::post('chirps', [ChirpController::class, 'store'])
+        ->name('chirps.store');
 });
 
 require __DIR__.'/auth.php';
